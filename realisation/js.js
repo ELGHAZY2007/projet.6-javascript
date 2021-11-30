ef069cd2090e483bbaa190477be878341c3f95b3
ELGHAZY MOHAMED REDA <elghazy.mohamedreda@gmail.com>
2021-11-30 12:43:59 +0100

    up

---
+++ realisation/js.js
@@ -0,0 +1,40 @@
+// DECLARATION DES VARIABLES 
+
+var guess = document.getElementById("submitGuess")
+var message = document.getElementById("output")
+var tentatives = 10;
+var number = Math.floor(Math.random() * 100)
+var input = document.getElementById('guess').value;
+
+// TRAITEMENT  
+
+ function submit() {
+
+    tentatives--;
+    if (tentatives > 0) {
+    
+        if (input == number){
+            if (tentatives >= 8) {
+                message="Bravo, vous etes un Génie !!!"
+
+            }   else {
+                    message = "Félicitations, vous avez gagné après " +(10-tentatives)+ " tentatives";
+                }
+        } 
+        
+        else if (input > number ) {
+            message= "Essayer un petit nombre !"
+
+        } 
+
+        else  {
+            message= "Essayer un grand nombre !"
+        }
+    } else {
+         message="c'est raté"
+     }
+
+// AFFICHAGE
+
+    output.innerHTML=message
+}
